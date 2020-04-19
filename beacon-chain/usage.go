ebbeae8e5e27a14248e116b53349e2d75719c7de
terence tsao <terence@prysmaticlabs.com>
2020-04-19 10:04:26 -0700

    Revert "Deprecate archival service (#5445)"
    This reverts commit 4fbcedf5415505833f37bb557107519f04d33e86.

--- beacon-chain/usage.go
+++ beacon-chain/usage.go
@@ -131,6 +131,15 @@ var appHelpFlagGroups = []flagGroup{
 			flags.InteropNumValidatorsFlag,
 		},
 	},
+	{
+		Name: "archive",
+		Flags: []cli.Flag{
+			flags.ArchiveEnableFlag,
+			flags.ArchiveValidatorSetChangesFlag,
+			flags.ArchiveBlocksFlag,
+			flags.ArchiveAttestationsFlag,
+		},
+	},
 }
 
 func init() {
