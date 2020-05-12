6d1501355ad1aaf80f5442939d7e10b20fab12db
Preston Van Loon <preston@prysmaticlabs.com>
2020-05-12 16:09:57 -0700

    disable cache

--- fuzz/block_fuzz.py
+++ fuzz/block_fuzz.py
@@ -1,18 +1,10 @@
-import os
-import sys
-
 from eth2spec.debug import decode
 from eth2spec.phase0 import spec
 from eth2spec.utils import bls
 from eth2spec.utils.ssz.ssz_impl import serialize
-from eth2spec.utils.ssz.ssz_typing import uint8, uint32
-# from preset_loader import loader
-#
-# # TODO fix up so not hard-coded
-# configs_path = "/eth2/eth2.0-specs/configs"
-# # Apply 'mainnet' template
-# presets = loader.load_presets(configs_path, "mainnet")
-# spec.apply_constants_preset(presets)
+
+# Disable cache
+spec.hash = spec._hash
 
 VALIDATE_STATE_ROOT = True
 
@@ -20,11 +12,9 @@ class BlockTestCase(spec.Container):
     pre: spec.BeaconState
     block: spec.SignedBeaconBlock
 
-
 def FuzzerInit(bls_disabled: bool) -> None:
     if bls_disabled:
         bls.bls_active = False
-
 
 def FuzzerRunOne(fuzzer_input):
     state_block = decode(fuzzer_input, BlockTestCase)
